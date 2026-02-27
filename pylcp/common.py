f7aa970a02e9cbdfe7531550980307ad5a1c014f
Johan Palm <johan.palm2000@gmail.com>
2026-02-27 10:32:32 +0100

    jnp rewrite for functions and running. Not fixed documentations

--- pylcp/common.py
+++ pylcp/common.py
@@ -1,7 +1,7 @@
 import time
 import copy
-import numpy as np
-from scipy.optimize import fsolve
+import jax.numpy as jnp
+import jax
 
 class progressBar(object):
     def __init__(self, decimals=1, fill='█', prefix='Progress:',
@@ -64,14 +64,13 @@ class progressBar(object):
 
 
 def cart2spherical(A):
-    return np.array([(A[0]-1j*A[1])/np.sqrt(2), A[2], -(A[0]+1j*A[1])/np.sqrt(2)])
+    return jnp.array([(A[0]-1j*A[1])/jnp.sqrt(2), A[2], -(A[0]+1j*A[1])/jnp.sqrt(2)])
 
 def spherical2cart(A):
-    return np.array([1/np.sqrt(2)*(-A[2]+A[0]), 1j/np.sqrt(2)*(A[2]+A[0]), A[1]])
+    return jnp.array([1/jnp.sqrt(2)*(-A[2]+A[0]), 1j/jnp.sqrt(2)*(A[2]+A[0]), A[1]])
 
 def spherical_dot(A, B):
-    return np.tensordot(A, np.array([-1., 1., -1.])*B[::-1], axes=(0, 0))
-    #return np.tensordot(A, np.conjugate(B), axes=(0,0))
+    return jnp.tensordot(A, jnp.array([-1., 1., -1.])*B[::-1], axes=(0, 0))
 
 class base_force_profile():
     """
@@ -100,10 +99,10 @@ class base_force_profile():
         Equilibrium population found.
     """
     def __init__(self, R, V, laserBeams, hamiltonian):
-        if not isinstance(R, np.ndarray):
-            R = np.array(R)
-        if not isinstance(V, np.ndarray):
-            V = np.array(V)
+        if not isinstance(R, jnp.ndarray):
+            R = jnp.array(R)
+        if not isinstance(V, jnp.ndarray):
+            V = jnp.array(V)
 
         if R.shape[0] != 3 or V.shape[0] != 3:
             raise TypeError('R and V must have first dimension of 3.')
@@ -114,75 +113,98 @@ class base_force_profile():
         if hamiltonian is None:
             self.Neq = None
         else:
-            self.Neq = np.zeros(R[0].shape + (hamiltonian.n,))
+            self.Neq = jnp.zeros(R[0].shape + (hamiltonian.n,))
 
         self.f = {}
         for key in laserBeams:
-            self.f[key] = np.zeros(R.shape + (len(laserBeams[key].beam_vector),))
+            self.f[key] = jnp.zeros(R.shape + (len(laserBeams[key].beam_vector),))
 
-        self.f_mag = np.zeros(R.shape)
+        self.f_mag = jnp.zeros(R.shape)
 
-        self.F = np.zeros(R.shape)
+        self.F = jnp.zeros(R.shape)
 
     def store_data(self, ind, Neq, F, F_laser, F_mag):
         if not Neq is None:
-            self.Neq[ind] = Neq
+            self.Neq = self.Neq.at[ind].set(Neq)
 
         for jj in range(3):
-            #self.f[(jj,) + ind] = f[jj]
-            self.F[(jj,) + ind] = F[jj]
+            self.F = self.F.at[(jj,) + ind].set(F[jj])
             for key in F_laser:
-                self.f[key][(jj,) + ind] = F_laser[key][jj]
+                self.f[key] = self.f[key].at[(jj,) + ind].set(F_laser[key][jj])
 
-            self.f_mag[(jj,) + ind] = F_mag[jj]
+            self.f_mag = self.f_mag.at[(jj,) + ind].set(F_mag[jj])
+            
 
 
-def random_vector(rng, free_axes=[True, True, True]):
+def random_vector(key, free_axes=[True, True, True]):
     """
     This function returns a random vector in either 1D, 2D or 3D
 
     Parameters:
     -----------
-        rng: numpy.random.Generator()
-            Properly seeded random number generator.
+        key: jax.random.PRNGKeyArray
+            A JAX pseudo-random number generator key. Because JAX functions 
+            must be pure and stateless for GPU acceleration, randomness requires 
+        passing an explicit PRNG state.
         free_axis: list of 3 boolean (option)
             Which axes (x, y, z) are considered free axes.  Default:
             [True, True, True], i.e., a 3D vector.
 
     Returns
     -------
-        vector: array_like of shape (3,)
+        vector: jax.Array of shape (3,)
             Random vector with unit length.
     """
-    if np.sum(free_axes)==1:
-        return (np.sign(rng.random(1)-0.5)*free_axes).astype('float64')
-    elif np.sum(free_axes)==2:
-        phi = 2*np.pi*rng.random(1)[0]
-        x = np.array([np.cos(phi), np.sin(phi)])
-        y =np.zeros((3,))
-        y[free_axes] = x
+    free_axes_arr = jnp.array(free_axes)
+    axes_count = jnp.sum(free_axes_arr)
+    
+    key1, key2 = jax.random.split(key)
+    a = jax.random.uniform(key1)
+    b = jax.random.uniform(key2)
+    
+    if axes_count == 1:
+        val = jnp.sign(a - 0.5)
+        return (val * free_axes_arr).astype("float64")
+    
+    elif axes_count == 2:
+        phi = 2*jnp.pi*a
+        x = jnp.array([jnp.cos(phi), jnp.sin(phi)])
+        y = jnp.zeros(3)
+        y = y.at[free_axes_arr].set(x)
         return y
-    elif np.sum(free_axes)==3:
-        a, b = rng.random(2)
-        th = np.arccos(2*b-1)
-        phi = 2*np.pi*a
-
-        return np.array([np.sin(th)*np.cos(phi), np.sin(th)*np.sin(phi),
-                         np.cos(th)])
+    
+    elif axes_count == 3:
+        th = jnp.arccos(2*b - 1)
+        phi = 2*jnp.pi*a
+        return jnp.array([
+            jnp.sin(th)*jnp.cos(phi),
+            jnp.sin(th)*jnp.sin(phi),
+            jnp.cos(th)
+        ])
+    
     else:
-        raise StandardError('free_axes must be a boolean array of length 3.')
+        raise ValueError(f"free_axis must be a boolean array of length 1 to 3. free_axis is of length {axes_count}!")
+    
+    
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
+    import numpy as np
 
-    vectors = []
-    for n in range(500):
-        vectors.append(random_vector([True, True, True]))
-
+    master_key = jax.random.PRNGKey(42) # can be a random seed as well, is kept as a chosen number here
+    
+    keys = jax.random.split(master_key, 500) # splits the random key 500 times
+    
+    batched_random_vector = jax.vmap(random_vector, in_axes=(0, None))
+    
+    vectors = batched_random_vector(keys, (True, True, True)) # generates all random vectors at once
+    
     vectors = np.array(vectors)
+    
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(vectors[:,0], vectors[:,1], vectors[:,2])
     ax.view_init(elev=-90., azim=0.)
+    plt.show()
